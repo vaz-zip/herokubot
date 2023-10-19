c7360b96fe1675b936a98cb97e853f8e58683de3
vaz-zip <baldeha@gmail.com>
2023-10-19 13:47:08 +0400

    'Фиксация 3'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+# from telebot import types
 from config import TOKEN
 from extension import APIException
 
@@ -7,33 +8,76 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def helper(message: telebot.types.Message):
-    text = 'Введите необходимое количество буханок!'
-    bot.reply_to(message, text)
+    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}!</u></b>\
+        \nВведите номер рецептуры '
+    bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 @bot.message_handler(content_types=['text', ])
-def convert(message: telebot.types.Message):
-    try:
-        values = message.text.split(' ')
-        if len(values) != 1:
-            raise APIException('Знвчение должно быть одно!')
-        persons = values
+def get_user_text(message):
+    if message.text == '1': 
+        mess = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №1, введите количество буханок</u></b>'
+        bot.send_message(message.chat.id, mess, parse_mode='html')
+    elif message.text == '2':
+        mess1 = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №2</u></b>'
+        bot.send_message(message.chat.id, mess1, parse_mode='html')
+    elif message.text == '3':
+        mess2 = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №2, Введи чило булок</u></b>'
+        bot.send_message(message.chat.id, mess2, parse_mode='html')
+        # try:
+        #     values = message.text.split(' ')
+        #     if len(values) != 1:
+        #         raise APIException('Значение должно быть одно!')
+        #     # persons = values
+        #     # person = int(''.join(map(str, persons)))
+        # except APIException as e:
+        #     bot.reply_to(message, f'Ошибка!\n{e}')
+        #     # except Exception as e:
+        #     #     bot.reply_to(message, f'Не удалось обработать команду!\nВведите целое число!\nОшибка: {e}')
+        #     # else:
+        #     #     text = f'Количество буханок: {person}\
+        #     #         \nМука В/С....{person * 125} грамм,\
+        #     #         \nМука 1с......{person * 102} грамм,\
+        #     #         \nВода............{person * 160} грамм,\
+        #     #         \nЗакваска....{person * 15} грамм,\
+        #     #         \nСоль............{person * 5} грамм,\
+        #     #         \nДобавки.....{person * 20} грамм,\
+        #     #         \nМВУУУ-ХА-ХА-ХА! Recept #3!!'
+        bot.send_message(message.chat.id, mess2, parse_mode='html')
+            # bot.send_message(message.chat.id, text)
+            
 
-        person = int(''.join(map(str, persons)))
-
-    except APIException as e:
-        bot.reply_to(message, f'Ошибка!\n{e}')
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду!\nВведите целое число!\nОшибка: {e}')
-    else:
-        text = f'Ha {person} буханок:\
-               \nМука В/С....{person * 125} грамм,\
-               \nМука 1с......{person * 102} грамм,\
-               \nВода............{person * 160} грамм,\
-               \nЗакваска....{person * 15} грамм,\
-               \nСоль............{person * 5} грамм,\
-               \nДобавки.....{person * 20} грамм.'
-        bot.send_message(message.chat.id, text)
 
 
-bot.polling()
+
+
+    
+   
+
+
+# @bot.message_handler(content_types=['text', ])
+# def convert(message: telebot.types.Message):
+#     try:
+#         values = message.text.split(' ')
+#         if len(values) != 1:
+#             raise APIException('Знвчение должно быть одно!')
+#         persons = values
+
+#         person = int(''.join(map(str, persons)))
+
+#     except APIException as e:
+#         bot.reply_to(message, f'Ошибка!\n{e}')
+#     except Exception as e:
+#         bot.reply_to(message, f'Не удалось обработать команду!\nВведите целое число!\nОшибка: {e}')
+#     else:
+#         text = f'Количество буханок: {person}\
+#                \nМука В/С....{person * 125} грамм,\
+#                \nМука 1с......{person * 102} грамм,\
+#                \nВода............{person * 160} грамм,\
+#                \nЗакваска....{person * 15} грамм,\
+#                \nСоль............{person * 5} грамм,\
+#                \nДобавки.....{person * 20} грамм,\
+#                \nМВУУУ-ХА-ХА-ХА! I`am CRAGY-DACK !!'
+#         bot.send_message(message.chat.id, text)
+
+bot.polling(non_stop=True)
