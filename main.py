fd6b16d11bc04497132ff2c5ef9b341ee252639c
vaz-zip <baldeha@gmail.com>
2023-10-19 16:26:55 +0400

    '2 commit'

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-# from telebot import types
+from telebot import types
 from config import TOKEN
 from extension import APIException
 
@@ -9,21 +9,32 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def helper(message: telebot.types.Message):
     mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}!</u></b>\
-        \nВведите номер рецептуры '
+        \nВведите 1 '
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
+@bot.message_handler(comands=['1'])
+def get_text(message: telebot.types.Message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Багет', collback_data = 'recept1'))
+    markup.add(types.InlineKeyboardButton('Багет', collback_data = 'recept2'))
+    markup.add(types.InlineKeyboardButton('Багет', collback_data = 'recept3'))
+    markup.add(types.InlineKeyboardButton('Багет', collback_data = 'recept4'))
+    markup.add(types.InlineKeyboardButton('Багет', collback_data = 'recept5'))
+    markup.add(types.InlineKeyboardButton('Багет', collback_data = 'recept6'))
+    bot.reply_to(message, reply_markup = markup)
 
-@bot.message_handler(content_types=['text', ])
-def get_user_text(message: telebot.types.Message):
-    if message.text == '1': 
-        mess = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №1, введите количество буханок</u></b>'
-        bot.send_message(message.chat.id, mess, parse_mode='html')
-    elif message.text == '2':
-        mess1 = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №2</u></b>'
-        bot.send_message(message.chat.id, mess1, parse_mode='html')
-    elif message.text == '3':
-        mess2 = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №2, Введи чило булок</u></b>'
-        bot.send_message(message.chat.id, mess2, parse_mode='html')
+
+# @bot.message_handler(content_types=['text', ])
+# def get_user_text(message: telebot.types.Message):
+#     if message.text == '1': 
+#         mess = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №1, введите количество буханок</u></b>'
+#         bot.send_message(message.chat.id, mess, parse_mode='html')
+#     elif message.text == '2':
+#         mess1 = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №2</u></b>'
+#         bot.send_message(message.chat.id, mess1, parse_mode='html')
+#     elif message.text == '3':
+#         mess2 = f'Привет, <b>{message.from_user.first_name}! <u> Я рецепт №2, Введи чило булок</u></b>'
+#         bot.send_message(message.chat.id, mess2, parse_mode='html')
 
             
 
