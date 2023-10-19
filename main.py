8b7a6f695ba955ef1a43211699fcf890338f5b0c
vaz-zip <baldeha@gmail.com>
2023-10-19 21:17:45 +0400

    '6 commit'

--- main.py
+++ main.py
@@ -9,37 +9,40 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def helper(message: telebot.types.Message):
     mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}!</u></b>\
-        \nВведите 1 '
+        \nВведите пароль '
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 @bot.message_handler(content_types=['text', ])
 def get_text(message):
-    if message.text == '1':
+    if message.text == '9999':
         mess = f'{message.from_user.first_name}, выберите нужный рецепт!'
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Багет'))
-        markup.add(types.InlineKeyboardButton('Тыквенный'))
-        markup.add(types.InlineKeyboardButton('Чиабатта'))
-        markup.add(types.InlineKeyboardButton('Зерновой'))
-        markup.add(types.InlineKeyboardButton('Фитнес'))
-        markup.add(types.InlineKeyboardButton('Бородинский'))
-        bot.reply_to(message, mess, reply_markup = markup)
-        bot.register_next_step_handler(message, on_click)
+        markup = types.ReplyKeyboardMarkup()
+        markup.add(types.KeyboardButton('Багет'))
+        markup.add(types.KeyboardButton('Тыквенный'))
+        markup.add(types.KeyboardButton('Чиабатта'))
+        markup.add(types.KeyboardButton('Зерновой'))
+        markup.add(types.KeyboardButton('Фитнес'))
+        markup.add(types.KeyboardButton('Бородинский'))
+        bot.send_message(message, mess, message.chat.id, reply_markup=markup)
+        # bot.reply_to(message, mess)
+        # bot.register_message_handler(message, convert)
 
-def on_click(message):
-    if message.text == 'Багет':
-        try:
-            values = message.text.split(' ')
-            if len(values) != 1:
-                raise APIException('Знвчение должно быть одно!')
-            persons = values
-            person = int(''.join(map(str, persons)))
-        except APIException as e:
-            bot.reply_to(message, f'Ошибка!\n{e}')
-        except Exception as e:
-            bot.reply_to(message, f'Не удалось обработать команду!\nВведите целое число!\nОшибка: {e}')
-        else:
-            text = f'Количество буханок: {person}\
+# @bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 1:
+            raise APIException('Знвчение должно быть одно!')
+        persons = values
+
+        person = int(''.join(map(str, persons)))
+
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка!\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду!\nВведите целое число!\nОшибка: {e}')
+    else:
+        text = f'Количество буханок: {person}\
                \nМука В/С....{person * 125} грамм,\
                \nМука 1с......{person * 102} грамм,\
                \nВода............{person * 160} грамм,\
@@ -47,8 +50,7 @@ def on_click(message):
                \nСоль............{person * 5} грамм,\
                \nДобавки.....{person * 20} грамм,\
                \nМВУУУ-ХА-ХА-ХА! I`am CRAGY-DACK !!'
-            bot.send_message(message.chat.id, text)        
-
+        bot.send_message(message.chat.id, text)
 # @bot.callback_query_handler(func=lambda callback: True)
 # def callback_message(callback):
 #     if callback.data == 'recept1':
